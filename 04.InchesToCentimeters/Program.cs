122a186a3bd465f9f8141be6918b5b99f96e0b50
Erhi05 <erhanaudi80@abv.bg>
2024-08-04 00:38:54 +0300

    Finished

--- 04.InchesToCentimeters/Program.cs
+++ 04.InchesToCentimeters/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double inch = double.Parse(Console.ReadLine());
+
+            double centimeters = inch * 2.54;
+            Console.WriteLine(centimeters);
         }
     }
 }
